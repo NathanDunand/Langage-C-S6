c79e95f9d0511285b8d05f0a1d024923fe7b04df
GUENNEC Paul <paul.guennec@protonmail.com>
2022-03-09 11:18:05 +0100

    don't working but saving files

--- TP1/src/exo6.c
+++ TP1/src/exo6.c
@@ -1,22 +1,58 @@
 #include <stdio.h>
 
 /**
- * Exercice 5 : Le modulo
+ * Exercice 6 : Opérateurs logiques et Booléens
  *
  * Ce programme effectue des calculs et affiche les valeurs
  **/
-
-/**
- * On est obligé de cast le nombre réel en int car l'opération modulo
- * ne peut être effectuée sur un nombre réel.
- **/
 int main(int argc, char **argv)
 {
-    int x = 5 % 2;
+    /**
+     * vaut 4 car 5 = 101 en base 2 et 6 = 110 en base 2 donc
+     * 101 & 110 = 100 qui vaut 4 en base 10
+     **/
+    int x = 5 & 6;
     printf("%i\n", x);
 
-    float x2 = (int)5.0 % 2;
-    printf("%f\n", x2);
+    /**
+     * vaut 1 car les nombres (sauf 0) valent tous 1 en booléen
+     * donc 1 & 1 = 1.
+     **/
+    x = 5 && 6;
+    printf("%i\n", x);
+
+    /**
+     * vaut 7 car 5 = 101 en base 2 et 6 = 110 en base 2 donc
+     * 101 | 110 = 111 qui vaut 7 en base 10
+     **/
+    x = 5 | 6;
+    printf("%i\n", x);
+
+    /**
+     * vaut 1 car les nombres (sauf 0) valent tous 1 en booléen
+     * donc 1 || 1 = 1.
+     **/
+    x = 5 || 6;
+    printf("%i\n", x);
+
+    /**
+     * vaut 3 car 5 = 101 en base 2 et 6 = 110 en base 2 donc
+     * 101 ^ 110 = 011 qui vaut 3 en base 10
+     **/
+    x = 5 ^ 6;
+    printf("%i\n", x);
+
+    /**
+     * inverse les bits 0 à 1 et 1 à 0. et on obtient donc le complémentaire de 5 qui est -6
+     **/
+    x = ~5;
+    printf("%i\n", x);
+
+    /**
+     * vaut 0 car 5 vaut 1 en booléen donc !1 = 0
+     **/
+    x = !5;
+    printf("%i\n", x);
 
     return 0;
 }
--- TP1/src/exo7.c
+++ TP1/src/exo7.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /**
- * Exercice 7 : Opérateurs logiques et Booléens
+ * Exercice 7 : Autour du cercle
  *
- * Ce programme effectue des calculs et affiche les valeurs
+ * Ce programme effectue des calculs concernant les cercles
+ * et affiche les valeurs obtenue (périmètre et surface)
  **/
+
+#define PI 3.14159
+
 int main(int argc, char **argv)
 {
-    int x = 5 & 6;
-    printf("%i\n", x);
+    if (argc != 2)
+    {
+        printf("Veuillez mettre un rayon de cercle");
+        return 1;
+    }
 
-    x = 5 && 6;
-    printf("%i\n", x);
+    float rayon = (float)strtod(argv[1], NULL);
+    float perimetre = PI * pow(rayon, 2);
+    printf("%f\n", perimetre);
 
     return 0;
 }
