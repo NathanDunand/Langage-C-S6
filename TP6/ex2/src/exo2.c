1f1bb918e65698de1000e5dc22a46e00227ff154
nathand <nathandunand@orange.fr>
2022-03-09 11:15:05 +0100

    Makefile

---
+++ TP6/ex2/src/exo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Exercice 3 : S'ouvrir au monde !
+ *
+ * Ce programme affiche "Hello world !" dans le terminal
+ **/
+int main(int argc, char **argv)
+{
+    printf("exo2-TP6");
+    return 0;
+}
