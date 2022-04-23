1f52199c8f2cae9910b0fc7d41abde308c121139
GUENNEC Paul <paul.guennec@protonmail.com>
2022-04-23 17:58:58 +0200

    le make file c'est chiant

---
+++ TP3/src/2-4/exo2-4.c
@@ -0,0 +1,6 @@
+#include "fonction.h"
+
+int main(int argc, char **argv)
+{
+    affiche_hello();
+}
---
+++ TP3/src/2-4/fonction.c
@@ -0,0 +1,6 @@
+#include "fonction.h"
+
+void affiche_hello()
+{
+    printf("Hello World\n");
+}
