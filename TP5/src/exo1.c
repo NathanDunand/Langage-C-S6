4c2a9fc2409f2a30b290c9cebf3d8c5b9ee43b09
Polgue <paul.guennec@protonmail.com>
2022-05-02 16:00:08 +0200

    tp5 part 1

---
+++ TP5/src/exo1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void) { 
+  int *px; px = (int *) malloc (sizeof(int)); 
+
+  *px = 1;  
+  printf("Adresse de px : %X\n", &px); 
+  printf("Adresse pointee par le pointeur px = %X\n", px);
+  printf("Valeur memorisee a l'adresse pointee par le pointeur px = %d\n\n", *px);
+}
