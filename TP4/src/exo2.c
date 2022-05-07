8895f7c60aed3f38e746d07d6dd8db43f0613d75
GUENNEC Paul <paul.guennec@protonmail.com>
2022-05-07 11:25:45 +0200

    Problem not fixed in "sort_by_date" funct

--- TP4/src/exo2.c
+++ TP4/src/exo2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LENGTH_ARRAY 20
 
@@ -38,10 +39,9 @@ char *charFromSTDIN(char *to_display)
         if (c == EOF)
             break;
 
-        if ((c < 48 || c > 57) && c != 10)
+        if (c == 10)
         {
-            printf("Veuillez rentrer des chiffres\n");
-            exit(1);
+            break;
         }
 
         if (--len == 0)
@@ -117,40 +117,112 @@ void create_personne()
 {
     personne new_personne;
     date new_date;
+    new_date.day = -1;
+    new_date.month = -1;
+    new_date.year = -1;
 
     new_personne.name = "";
     new_personne.surname = "";
 
+    char *name = charFromSTDIN("Entrer le nom : ");
+    char *surname = charFromSTDIN("Entrez le prénom : ");
+
+    new_personne.name = name;
+    new_personne.surname = surname;
+
     int day = intFromSTDIN("Entrer le jour de naissance : ");
     if (day < 1 || day > 31)
     {
-        printf("Nombre incorrect.");
+        printf("Nombre incorrect.\n");
         exit(1);
     }
-    new_personne.date_of_birth.day = day;
+    new_date.day = day;
 
     int month = intFromSTDIN("Entrer le numéro de mois de naissance : ");
-    if (day < 1 || day > 12)
+    if (month < 1 || month > 12)
     {
-        printf("Nombre incorrect.");
+        printf("Nombre incorrect.\n");
         exit(1);
     }
-    new_personne.date_of_birth.month = month;
+    new_date.month = month;
 
     int year = intFromSTDIN("Entrer l'année de naissance : ");
     if (year < 0 || year > 2022)
     {
-        printf("Nombre incorrect.");
+        printf("Nombre incorrect.\n");
         exit(1);
     }
-    new_personne.date_of_birth.year = year;
+    new_date.year = year;
 
     new_personne.date_of_birth = new_date;
     DATABASE[CURRENT_INDEX] = new_personne;
+    CURRENT_INDEX++;
+}
+
+void display_people()
+{
+    for (int i = 0; i < CURRENT_INDEX; i++)
+    {
+        printf("Nom : %s\nPrénom : %s\nDate Naissance : %d/%d/%d\n", DATABASE[i].name, DATABASE[i].surname, DATABASE[i].date_of_birth.day, DATABASE[i].date_of_birth.month, DATABASE[i].date_of_birth.year);
+        printf("###\n");
+    }
+}
+
+void sort_by_date() // error de bufferoverflow
+{
+    for (int i = 0; i < CURRENT_INDEX - 1; i++)
+    {
+        for (int j = i + 1; j < CURRENT_INDEX; j++)
+        {
+            char i_day[3];
+            char i_month[3];
+            char i_year[5];
+            char i_concat[3 + 3 + 5];
+            int i_cmp;
+
+            sprintf(i_day, "%d", DATABASE[i].date_of_birth.day);
+            sprintf(i_month, "%d", DATABASE[i].date_of_birth.month);
+            sprintf(i_year, "%d", DATABASE[i].date_of_birth.year);
+
+            strcat(i_concat, i_day);
+            strcat(i_concat, i_month);
+            strcat(i_concat, i_year);
+
+            i_cmp = atoi(i_concat);
+
+            char j_day[3];
+            char j_month[3];
+            char j_year[5];
+            char j_concat[3 + 3 + 5];
+            int j_cmp;
+
+            sprintf(j_day, "%d", DATABASE[j].date_of_birth.day);
+            sprintf(j_month, "%d", DATABASE[j].date_of_birth.month);
+            sprintf(j_year, "%d", DATABASE[j].date_of_birth.year);
+
+            strcat(j_concat, j_day);
+            strcat(j_concat, j_month);
+            strcat(j_concat, j_year);
+
+            j_cmp = atoi(j_concat);
+
+            if (i_cmp > j_cmp)
+            {
+                personne tmp = DATABASE[i];
+                DATABASE[i] = DATABASE[j];
+                DATABASE[j] = tmp;
+            }
+        }
+    }
 }
 
 int main()
 {
+    create_personne();
+    create_personne();
+    create_personne();
+    sort_by_date();
+    display_people();
 
     return 0;
 }
